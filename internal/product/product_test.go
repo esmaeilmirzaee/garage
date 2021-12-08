f88edb570370f617329aad003b64588eaec2fc8b
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-12-08 22:53:16 +0330

    Seeds the testing database and retrieve and checks for the length of data

--- internal/product/product_test.go
+++ internal/product/product_test.go
@@ -3,11 +3,13 @@ package product_test
 import (
 	"github.com/esmaeilmirzaee/grage/internal/platform/database/databasetest"
 	"github.com/esmaeilmirzaee/grage/internal/product"
+	"github.com/esmaeilmirzaee/grage/schema"
 	"github.com/google/go-cmp/cmp"
 	"testing"
 	"time"
 )
 
+// TestProducts creates and restore a new Product from the testing database
 func TestProducts(t *testing.T) {
 	db, cleanup := databasetest.Setup(t)
 	defer cleanup()
@@ -35,4 +37,24 @@ func TestProducts(t *testing.T) {
 		t.Fatalf("Stored and provided product mismatch. see diff: %v\n", diff)
 	}
 
+}
+
+// TestList uses seed to function to seed the testing database and finally checks
+// the length of seed and retrieved Products
+func TestList(t *testing.T) {
+	db, cleanup := databasetest.Setup(t)
+	defer cleanup()
+
+	if err := schema.Seed(db); err != nil {
+		t.Fatalf("Could not seed the testing database. %v", err)
+	}
+
+	ps, err := product.List(db)
+	if err != nil {
+		t.Fatalf("Could not retrieve data from testing database %v", err)
+	}
+
+	if exp, got := 2, len(ps); exp != got {
+		t.Fatalf("Expected %v but got %v", exp, got)
+	}
 }
