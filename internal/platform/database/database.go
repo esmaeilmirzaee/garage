9af8d737a7ccb2c3b6fc39d498bb5e72164a4529
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-12-13 19:10:15 +0330

    Status check the database function

--- internal/platform/database/database.go
+++ internal/platform/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"context"
 	"github.com/jmoiron/sqlx"
 	"net/url"
 
@@ -8,11 +9,11 @@ import (
 )
 
 type Config struct {
-	Host		string
-	Name		string
-	User		string
-	Password	string
-	DisableTLS	bool
+	Host       string
+	Name       string
+	User       string
+	Password   string
+	DisableTLS bool
 }
 
 // Open knows how to open a database connection
@@ -27,12 +28,25 @@ func Open(cfg Config) (*sqlx.DB, error) {
 	}
 
 	u := url.URL{
-		Scheme: "postgres",
-		User: url.UserPassword(cfg.User, cfg.Password),
-		Host: cfg.Host,
-		Path: cfg.Name,
+		Scheme:   "postgres",
+		User:     url.UserPassword(cfg.User, cfg.Password),
+		Host:     cfg.Host,
+		Path:     cfg.Name,
 		RawQuery: q.Encode(),
 	}
 
 	return sqlx.Open("postgres", u.String())
+}
+
+// StatusCheck returns nil if it can successfully talk to
+// the database. It returns a non-nil error otherwise.
+func StatusCheck(ctx context.Context, db *sqlx.DB) error {
+	// Run a simple query to determine connectivity. The db
+	// has a "Ping" method, but it can return false-positive when it
+	// was previously able to talk to the database but the database
+	// has since gone away. Running this query forces a round
+	// trip to the database.
+	var tmp bool
+	const q = `SELECT true`
+	return db.QueryRowContext(ctx, q).Scan(&tmp)
 }
